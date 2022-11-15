8c63760cf46bb3ab99b2e827e7b089c392470e9e
mlietti1 <mcf.lietti@outlook.com>
2022-11-15 15:48:43 +0100

    first commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>F</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>M</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
